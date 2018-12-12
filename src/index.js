2d3d91b1309b2c84f5b70fc6bf49433b7ca3bbe4
hupian <hupian@didichuxing.com>
2018-12-12 18:12:46 +0800

    替换 react-router 为 react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router } from 'react-router';
 import Routes from './routes';
 
 import './styles/common.scss';
 
-ReactDom.render(<Router> <Routes /> </Router>,  document.getElementById('myApp'));
+ReactDom.render(<Routes />,  document.getElementById('myApp'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function Routes() {
-  return <div>Here are my routes definition </div>
+  return <Router>
+    <Switch>
+      <Route path='/' exact render={(props)=><div {...props}> home page</div>} />
+      <Route path='/test' render={props=><div {...props}>test page</div>}/>
+    </Switch>
+  </Router>
 }
